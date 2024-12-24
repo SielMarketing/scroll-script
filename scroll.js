bb10e31febc9add2d110a46ce1434a4c4ee3ae54
SielMarketing <sielmarketing@gmail.com>
2024-12-25 01:45:59 +0400

    Add files via upload

--- scroll.js
+++ scroll.js
@@ -1,1 +1,8 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.location.hash) {
+        const target = document.querySelector(window.location.hash);
+        if (target) {
+            target.scrollIntoView({ behavior: "smooth" });
+        }
+    }
+});
